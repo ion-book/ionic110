d95ad8a5d21ae705168b21beac99ad10a46d3863
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2016-03-02 16:34:54 -0500

    updating blank template

--- slidebox/www/js/app.js
+++ slidebox/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('SlideboxModule', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,3 +22,20 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+.controller('SlideboxController', function($scope) {
+$scope.swiperOptions = {
+/* Whatever options */
+effect: 'coverflow',
+initialSlide: 0,
+direction:'vertical',
+speed: 100,
+autoplayStopOnLast: true,
+/* Initialize a scope variable with the swiper */
+onInit: function(swiper){
+$scope.swiper = swiper;
+},
+onSlideChangeEnd: function(swiper){
+console.log('The active index is ' + swiper.activeIndex); 
+}
+};
+});
